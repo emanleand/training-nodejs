d01c5292528f8ccfcf50600c105f24d2b2f06bb2
emanleand <emanleand@gmail.com>
2021-03-03 17:15:56 -0300

    exercise-student-approve

---
+++ exercise-note-student/student.js
@@ -0,0 +1,110 @@
+class Student {
+    constructor() {
+        this.students = [
+            {
+                name: 'james',
+                lastname: 'swaw',
+                score: 4
+            },
+            {
+                name: 'evelyn',
+                lastname: 'do',
+                score: 2
+            },
+            {
+                name: 'jonh',
+                lastname: 'jackson',
+                score: 8
+            },
+            {
+                name: 'ROBERT',
+                lastname: '',
+                score: 8
+            }
+        ];
+    }
+
+    forListApprovedStudent(note) {
+        let approved = [];
+        for (let i = 0; i < this.students.length; i++) {
+            if (this.students[i].score >= note) {
+                let fullName = this.__capitalize(this.students[i]);
+
+                delete this.students[i].lastname;
+                delete this.students[i].name;
+                this.students[i].fullName = fullName;
+
+                approved.push(this.students[i]);
+            }
+        }
+        //returned list of students ordered in ascending order
+        approved.sort(function (a, b) {
+            return a.score - b.score;
+        });
+
+        return approved;
+    }
+
+    whileListApprovedStudent(note) {
+        let approved = [];
+        let i = 0;
+        while (i < this.students.length) {
+            if (this.students[i].score >= note) {
+                let fullName = this.__capitalize(this.students[i]);
+
+                delete this.students[i].lastname;
+                delete this.students[i].name;
+                this.students[i].fullName = fullName;
+
+                approved.push(this.students[i]);
+            }
+            i++;
+        }
+        //returned list of students ordered in ascending order
+        approved.sort(function (a, b) {
+            return a.score - b.score;
+        });
+
+        return approved;
+    }
+
+    doWhileListApprovedStudent(note) {
+        let approved = [];
+        let i = 0;
+        do {
+            if (this.students[i].score >= note) {
+                let fullName = this.__capitalize(this.students[i]);
+
+                delete this.students[i].lastname;
+                delete this.students[i].name;
+                this.students[i].fullName = fullName;
+
+                approved.push(this.students[i]);
+            }
+            i++;
+        } while (i < this.students.length);
+
+        //returned list of students ordered in ascending order
+        approved.sort(function (a, b) {
+            return a.score - b.score;
+        });
+
+        return approved;
+    }
+
+    __capitalize(student) {
+        let name = student.name.charAt(0).toUpperCase() +
+            student.name.toLowerCase().slice(1);
+        let lastname = student.lastname.charAt(0).toUpperCase() +
+            student.lastname.toLowerCase().slice(1);
+
+        let fullName = name + ' ' + lastname;
+
+        return fullName.trim();
+    }
+}
+
+let student = new Student();
+console.log(student.forListApprovedStudent(2));
+//console.log(student.whileListApprovedStudent(8));
+//console.log(student.doWhileListApprovedStudent(1));
